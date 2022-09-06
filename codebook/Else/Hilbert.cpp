92b2de333f3b905176c3310bbde376bef090a0c8
abc864197532 <kimi930130@gmail.com>
2022-09-06 22:27:14 +0800

    Update Basic & Else

---
+++ codebook/Else/Hilbert.cpp
@@ -0,0 +1,14 @@
+long long hilbertOrder(int x, int y, int pow, int rotate) {
+    if (pow == 0) return 0;
+    int hpow = 1 << (pow-1);
+    int seg = (x < hpow) ? ((y < hpow) ? 0 : 3) : ((y < hpow) ? 1 : 2);
+    seg = (seg + rotate) & 3;
+    const int rotateDelta[4] = {3, 0, 0, 1};
+    int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
+    int nrot = (rotate + rotateDelta[seg]) & 3;
+    long long subSquareSize = 1ll << (pow * 2 - 2);
+    long long ans = seg * subSquareSize;
+    long long add = hilbertOrder(nx, ny, pow - 1, nrot);
+    ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
+    return ans;
+}
