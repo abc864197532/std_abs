1370432838ec2532664809c0b205465e4bbda066
abc864197532 <kimi930130@gmail.com>
2022-09-21 01:10:36 +0800

    Update Flow

--- codebook/FlowMatching/MCMF.cpp
+++ codebook/FlowMatching/MCMF.cpp
@@ -1,17 +1,20 @@
+template <typename T>
 struct MCMF {
-    const int INF = 1 << 30;
+    const T INF = 1ll << 60;
     struct edge {
-        int v, f, c;
-        edge (int _v, int _f, int _c) : v(_v), f(_f), c(_c) {}
+        int v;
+        T f, c;
+        edge (int _v, T _f, T _c) : v(_v), f(_f), c(_c) {}
     };
     vector <edge> E;
     vector <vector <int>> adj;
-    vector <int> dis, pot, rt;
+    vector <T> dis, pot;
+    vector <int> rt;
     int n, s, t;
     MCMF (int _n, int _s, int _t) : n(_n), s(_s), t(_t) {
         adj.resize(n);
     }
-    void add_edge(int u, int v, int f, int c) {
+    void add_edge(int u, int v, T f, T c) {
         adj[u].pb(E.size()), E.pb(edge(v, f, c));
         adj[v].pb(E.size()), E.pb(edge(u, 0, -c));
     }
@@ -32,7 +35,7 @@ struct MCMF {
     }
     bool dijkstra() {
         rt.assign(n, -1), dis.assign(n, INF);
-        priority_queue <pair <int, int>, vector <pair <int, int>>, greater <pair <int, int>>> pq;
+        priority_queue <pair <T, int>, vector <pair <T, int>>, greater <pair <T, int>>> pq;
         dis[s] = 0, pq.emplace(dis[s], s);
         while (!pq.empty()) {
             int d, v; tie(d, v) = pq.top(); pq.pop();
@@ -44,15 +47,15 @@ struct MCMF {
         }
         return dis[t] != INF;
     }
-    pair <int, int> runFlow() {
+    pair <T, T> runFlow() {
         pot.assign(n, 0);
-        int cost = 0, flow = 0;
+        T cost = 0, flow = 0;
         bool fr = true;
         while ((fr ? SPFA() : dijkstra())) {
             for (int i = 0; i < n; i++) {
                 dis[i] += pot[i] - pot[s];
             }
-            int add = INF;
+            T add = INF;
             for (int i = t; i != s; i = E[rt[i] ^ 1].v) {
                 add = min(add, E[rt[i]].f);
             }
