89c6c0ed4d3e18679e2982126b1481825596b6bd
fmb123456 <79188860+fmb123456@users.noreply.github.com>
2022-09-20 10:49:21 +0800

    Update Graph and DS

---
+++ codebook/Data-Structure/2D_seg.cpp
@@ -0,0 +1,74 @@
+// 2D range add, range sum in log^2
+struct seg {
+    int l, r;
+    ll sum, lz;
+    seg *ch[2]{};
+    seg(int _l, int _r) : l(_l), r(_r), sum(0), lz(0) {}
+    void push() {
+        if (lz)
+            ch[0]->add(l, r, lz), ch[1]->modify(l, r, lz), lz = 0;
+    }
+    void pull() {
+        sum = ch[0]->sum + ch[1]->sum;
+    }
+    void add(int _l, int _r, ll d) {
+        if (_l <= l && r <= _r) {
+            sum += d * (r - l);
+            lz += d;
+            return;
+        }
+        if (!ch[0])
+            ch[0] = new seg(l, l + r >> 1), ch[1] = new seg(l + r >> 1, r);
+        push();
+        if (_l < l + r >> 1)
+            ch[0]->add(_l, _r, d);
+        if (l + r >> 1 < _r)
+            ch[1]->add(_l, _r, d);
+        pull();
+    }
+    ll qsum(int _l, int _r) {
+        if (_l <= l && r <= _r)
+            return sum;
+        if (!ch[0])
+            return lz * (min(r, _r) - max(l, _l));
+        push();
+        ll res = 0;
+        if (_l < l + r >> 1)
+            res += ch[0]->qsum(_l, _r);
+        if (l + r >> 1 < _r)
+            res += ch[1]->qsum(_l, _r);
+        return res;
+    }
+};
+
+struct seg2 {
+    int l, r;
+    seg v, lz;
+    seg2 *ch[2]{};
+    seg2(int _l, int _r) : l(_l), r(_r), v(0, N), lz(0, N) {
+        if (l < r - 1)
+            ch[0] = new seg2(l, l + r >> 1), ch[1] = new seg2(l + r >> 1, r);
+    }
+    void add(int _l, int _r, int _l2, int _r2, ll d) {
+        v.add(_l2, _r2, d * (min(r, _r) - max(l, _l)));
+        if (_l <= l && r <= _r) {
+            lz.add(_l2, _r2, d);
+            return;
+        }
+        if (_l < l + r >> 1)
+            ch[0]->add(_l, _r, _l2, _r2, d);
+        if (l + r >> 1 < _r)
+            ch[1]->add(_l, _r, _l2, _r2, d);
+    }
+    ll qsum(int _l, int _r, int _l2, int _r2) {
+        ll res = v.qsum(_l2, _r2);
+        if (_l <= l && r <= _r)
+            return res;
+        res += lz.qsum(_l2, _r2) * (min(r, _r) - max(l, _l));
+        if (_l < l + r >> 1)
+            res += ch[0]->query(_l, _r, _l2, _r2);
+        if (l + r >> 1 < _r)
+            res += ch[1]->query(_l, _r, _l2, _r2);
+        return res;
+    }
+};
