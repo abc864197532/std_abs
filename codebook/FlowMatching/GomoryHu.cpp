363d31d757695c5f18342ae8562148ba6389e6d8
abc864197532 <kimi930130@gmail.com>
2022-09-21 09:48:29 +0800

    Create GomoryHu.cpp

---
+++ codebook/FlowMatching/GomoryHu.cpp
@@ -0,0 +1,19 @@
+vector <array <int, 3>> GomoryHu(vector <vector <pii>> adj, int n) {
+    Dinic flow(n); // 0-based
+    for (int i = 0; i < n; ++i) for (auto [j, w] : adj[i])
+        flow.add_edge(i, j, w);
+    flow.record();
+    vector <array <int, 3>> ans;
+    vector <int> rt(n);
+    for (int i = 0; i < n; ++i) rt[i] = 0;
+    for (int i = 1; i < n; ++i) {
+        int t = rt[i];
+        flow.reset(); // clear flows on all edge
+        ans.push_back({i, t, flow.solve(i, t)});
+        flow.runbfs(i);
+        for (int j = i + 1; j < n; ++j) if (rt[j] == t && flow.vis[j]) {
+            rt[j] = i;
+        }
+    }
+    return ans;
+}
