0a4bb2df61c397d0f43cc99882a1157a4b47f98f
abc864197532 <kimi930130@gmail.com>
2022-09-26 17:58:12 +0800

    Update Stress.sh

--- codebook/Basic/Stress.sh
+++ codebook/Basic/Stress.sh
@@ -1,10 +1,12 @@
-g++ main.cpp -o main
-g++ gen.cpp -o gen
-g++ good.cpp -o good
+g++ $1.cpp -o $1
+g++ $2.cpp -o $2
+g++ $3.cpp -o $3
 for i in {1..100} ; do
-  ./gen > input.txt
-  ./main < input.txt > output1.txt
-  ./good < input.txt > output2.txt
+  ./$3 > input.txt
+  # st=$(date +%s%N)
+  ./$1 < input.txt > output1.txt
+  # echo "$((($(date +%s%N) - $st)/1000000))ms"
+  ./$2 < input.txt > output2.txt
   if cmp --silent -- "output1.txt" "output2.txt" ; then
     continue
   fi
@@ -17,3 +19,4 @@ for i in {1..100} ; do
   break
 done
 echo OK!
+./stress.sh main good gen
