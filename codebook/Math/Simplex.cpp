a6d18d009d0afc3ac40907c6b2ee6c8c6120811f
abc864197532 <kimi930130@gmail.com>
2022-09-26 17:58:18 +0800

    Create Simplex.cpp

---
+++ codebook/Math/Simplex.cpp
@@ -0,0 +1,60 @@
+struct Simplex { // 0-based
+  using T = long double;
+  static const int N = 410, M = 30010;
+  const T eps = 1e-7;
+  int n, m;
+  int Left[M], Down[N];
+  // Ax <= b, max c^T x
+  // result : v, xi = sol[i]. 1 based
+  T a[M][N], b[M], c[N], v, sol[N];
+  bool eq(T a, T b) {return fabs(a - b) < eps;}
+  bool ls(T a, T b) {return a < b && !eq(a, b);}
+  void init(int _n, int _m) {
+    n = _n, m = _m, v = 0;
+    for (int i = 0; i < m; ++i) for (int j = 0; j < n; ++j) a[i][j] = 0;
+    for (int i = 0; i < m; ++i) b[i] = 0;
+    for (int i = 0; i < n; ++i) c[i] = sol[i] = 0;
+  }
+  void pivot(int x, int y) {
+    swap(Left[x], Down[y]);
+    T k = a[x][y]; a[x][y] = 1;
+    vector <int> nz;
+    for (int i = 0; i < n; ++i) {
+      a[x][i] /= k;
+      if (!eq(a[x][i], 0)) nz.push_back(i);
+    }
+    b[x] /= k;
+    for (int i = 0; i < m; ++i) {
+      if (i == x || eq(a[i][y], 0)) continue;
+      k = a[i][y], a[i][y] = 0;
+      b[i] -= k * b[x];
+      for (int j : nz) a[i][j] -= k * a[x][j];
+    }
+    if (eq(c[y], 0)) return;
+    k = c[y], c[y] = 0, v += k * b[x];
+    for (int i : nz) c[i] -= k * a[x][i];
+  }
+  // 0: found solution, 1: no feasible solution, 2: unbounded
+  int solve() {
+    for (int i = 0; i < n; ++i) Down[i] = i;
+    for (int i = 0; i < m; ++i) Left[i] = n + i;
+    while (1) {
+      int x = -1, y = -1;
+      for (int i = 0; i < m; ++i) if (ls(b[i], 0) && (x == -1 || b[i] < b[x])) x = i;
+      if (x == -1) break;
+      for (int i = 0; i < n; ++i) if (ls(a[x][i], 0) && (y == -1 || a[x][i] < a[x][y])) y = i;
+      if (y == -1) return 1;
+      pivot(x, y);
+    }
+    while (1) {
+      int x = -1, y = -1;
+      for (int i = 0; i < n; ++i) if (ls(0, c[i]) && (y == -1 || c[i] > c[y])) y = i;
+      if (y == -1) break;
+      for (int i = 0; i < m; ++i) if (ls(0, a[i][y]) && (x == -1 || b[i] / a[i][y] < b[x] / a[x][y])) x = i;
+      if (x == -1) return 2;
+      pivot(x, y);
+    }
+    for (int i = 0; i < m; ++i) if (Left[i] < n) sol[Left[i]] = b[i];
+    return 0;
+  }
+};
