6af0c8acfc0a6f9067cb6f8d799b386e2ec6b04d
abc864197532 <kimi930130@gmail.com>
2022-11-11 18:09:02 +0800

    update

--- codebook/FlowMatching/Dinic.cpp
+++ codebook/FlowMatching/Dinic.cpp
@@ -1,46 +1,59 @@
-struct Dinic {
-  const int INF = 1 << 30;
+struct Dinic { // 0-base
   struct edge {
-    int v, f;
-    edge (int _v, int _f) : v(_v), f(_f) {}
+    int to, cap, flow, rev;
   };
-  vector <vector <int>> adj;
-  vector <edge> E;
-  vector <int> level;
-  int n, s, t;
-  Dinic (int _n, int _s, int _t) : n(_n), s(_s), t(_t) {adj.resize(n);}
-  void add_edge(int u, int v, int f) {
-    adj[u].pb(E.size()), E.pb(edge(v, f));
-    adj[v].pb(E.size()), E.pb(edge(u, 0));
+  vector<edge> adj[N];
+  int s, t, dis[N], cur[N], n;
+  int dfs(int u, int cap) {
+    if (u == t || !cap) return cap;
+    for (int &i = cur[u]; i < (int)adj[u].size(); ++i) {
+      edge &e = adj[u][i];
+      if (dis[e.to] == dis[u] + 1 && e.flow != e.cap) {
+        int df = dfs(e.to, min(e.cap - e.flow, cap));
+        if (df) {
+          e.flow += df;
+          adj[e.to][e.rev].flow -= df;
+          return df;
+        }
+      }
+    }
+    dis[u] = -1;
+    return 0;
   }
   bool bfs() {
-    level.assign(n, -1);
-    queue <int> q;
-    level[s] = 0, q.push(s);
+    fill_n(dis, n, -1);
+    queue<int> q;
+    q.push(s), dis[s] = 0;
     while (!q.empty()) {
-      int v = q.front(); q.pop();
-      for (int id : adj[v]) if (E[id].f > 0 && level[E[id].v] == -1) {
-          level[E[id].v] = level[v] + 1;
-          q.push(E[id].v);
+      int tmp = q.front();
+      q.pop();
+      for (auto &u : adj[tmp])
+        if (!~dis[u.to] && u.flow != u.cap) {
+          q.push(u.to);
+          dis[u.to] = dis[tmp] + 1;
         }
     }
-    return level[t] != -1;
+    return dis[t] != -1;
   }
-  int dfs(int v, int minf) {
-    if (v == t) return minf;
-    int ans = 0;
-    for (int id : adj[v]) if (E[id].f > 0 && level[E[id].v] == level[v] + 1) {
-        int nxtf = dfs(E[id].v, min(minf, E[id].f));
-        minf -= nxtf, E[id].f -= nxtf;
-        ans += nxtf, E[id ^ 1].f += nxtf;
-        if (!minf) return ans;
-      }
-    if (!ans) level[v] = -1;
-    return ans;
+  int maxflow(int _s, int _t) {
+    s = _s, t = _t;
+    int flow = 0, df;
+    while (bfs()) {
+      fill_n(cur, n, 0);
+      while ((df = dfs(s, INF))) flow += df;
+    }
+    return flow;
   }
-  int solve() {
-    int ans = 0;
-    while (bfs()) ans += dfs(s, INF);
-    return ans;
+  void init(int _n) {
+    n = _n;
+    for (int i = 0; i < n; ++i) adj[i].clear();
+  }
+  void reset() {
+    for (int i = 0; i < n; ++i)
+      for (auto &j : adj[i]) j.flow = 0;
+  }
+  void add_edge(int u, int v, int cap) {
+    adj[u].pb(edge{v, cap, 0, (int)adj[v].size()});
+    adj[v].pb(edge{u, 0, 0, (int)adj[u].size() - 1});
   }
 };
--- codebook/Polynomial/NTT.cpp
+++ codebook/Polynomial/NTT.cpp
@@ -1,32 +1,35 @@
 const int N = 1 << 20, mod = 998244353, G = 3;
-void run (vector <ll> &P, bool inv = false) {
-  int N = P.size();
-  const ll w = modpow(G, (mod - 1) / N);
-  int lg = __lg(N);
-  vector <int> rev(N);
-  for (int i = 1; i < N; ++i) {
-    rev[i] = (rev[i >> 1] >> 1) | ((i & 1) << (lg - 1));
-    if (i < rev[i])
-      swap(P[i], P[rev[i]]);
+struct NTT {
+  ll w[N];
+  ll mpow(ll a, ll n);
+  NTT() {
+    ll dw = mpow(G, (mod - 1) / N);
+    w[0] = 1;
+    for (int i = 1; i < N; ++i) w[i] = w[i - 1] * dw % mod;
   }
-  vector <ll> ws = {inv ? modpow(w, mod - 2) : w};
-  for (int i = 1; i < lg; ++i) ws.push_back(ws.back() * ws.back() % mod);
-  reverse(ws.begin(), ws.end());
-  for (int i = 0; i < lg; ++i) {
-    for (int k = 0; k < N; k += (2 << i)) {
-      ll base = 1;
-      for (int j = k; j < k + (1 << i); ++j, base = base * ws[i] % mod) {
-        ll t = base * P[j + (1 << i)] % mod, u = P[j];
-        P[j] = u + t, P[j + (1 << i)] = u - t;
-        if (P[j] >= mod) P[j] -= mod;
-        if (P[j + (1 << i)] < 0) P[j + (1 << i)] += mod;
+  void bitrev(ll *a, int n) {
+    int i = 0;
+    for (int j = 1; j < n - 1; ++j) {
+      for (int k = n >> 1; (i ^= k) < k; k >>= 1);
+      if (j < i) swap(a[i], a[j]);
+    }
+  }
+  void operator()(ll *a, int n, bool inv = false) { //0 <= a[i] < P
+    bitrev(a, n);
+    for (int L = 2; L <= n; L <<= 1) {
+      int dx = N / L, dl = L >> 1;
+      for (int i = 0; i < n; i += L) {
+        for (int j = i, x = 0; j < i + dl; ++j, x += dx) {
+          ll tmp = a[j + dl] * w[x] % mod;
+          if ((a[j + dl] = a[j] - tmp) < 0) a[j + dl] += mod;
+          if ((a[j] += tmp) >= mod) a[j] -= mod;
+        }
       }
     }
-  }
-  if (inv) {
-    ll ninv = modpow(N, mod - 2);
-    for (int i = 0; i < N; ++i) {
-      P[i] = P[i] * ninv % mod;
+    if (inv) {
+      reverse(a + 1, a + n);
+      ll invn = modpow(n, mod - 2);
+      for (int i = 0; i < n; ++i) a[i] = a[i] * invn % mod;
     }
   }
-}
+};
