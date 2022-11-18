095c85886041e566b72935909bb09f9fe75c293b
abc864197532 <kimi930130@gmail.com>
2022-11-18 20:36:50 +0800

    update readme

--- codebook/Geometry/basic.cpp
+++ codebook/Geometry/basic.cpp
@@ -1,3 +1,4 @@
+const double eps = 1e-8, pi = acos(-1);
 int sign(double x) {return abs(x) <= eps ? 0 : (x > 0 ? 1 : -1);}
 struct Pt {
   double x, y;
@@ -9,6 +10,12 @@ struct Pt {
   double operator * (Pt o) {return x * o.x + y * o.y;}
   double operator ^ (Pt o) {return x * o.y - y * o.x;}
 };
+struct Line {
+  Pt a, b;
+};
+struct Cir {
+  Pt o; double r;
+};
 double abs2(Pt o) {return o.x * o.x + o.y * o.y;}
 double abs(Pt o) {return sqrt(abs2(o));}
 int ori(Pt o, Pt a, Pt b) {return sign((o - a) ^ (o - b));}
@@ -16,15 +23,14 @@ bool btw(Pt a, Pt b, Pt c) { // c on segment ab?
   return ori(a, b, c) == 0 && sign((c - a) * (c - b)) <= 0;
 }
 double area(Pt a, Pt b, Pt c) {return abs((a - b) ^ (a - c)) / 2;}
+Pt unit(Pt o) {return o / abs(o);}
+Pt rot(Pt a, double o) { // CCW
+  double c = cos(o), s = sin(o);
+  return Pt(c * a.x - s * a.y, s * a.x + c * a.y);
+}
 Pt proj_vector(Pt a, Pt b, Pt c) { // vector ac proj to ab
   return (b - a) * ((c - a) * (b - a)) / ((b - a) * (b - a));
 }
 Pt proj_pt(Pt a, Pt b, Pt c) { // point c proj to ab
   return proj_vector(a, b, c) + a;
 }
-struct Line {
-  Pt a, b;
-};
-struct Cir {
-  Pt o; double r;
-};
