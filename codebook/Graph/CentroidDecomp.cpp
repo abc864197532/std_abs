45fc1c05eb88e27443b5f1b2ca969e4bee9c82d0
abc864197532 <kimi930130@gmail.com>
2023-01-28 00:16:19 +0800

    Fix CentroidDecomp.cpp

--- codebook/Graph/CentroidDecomp.cpp
+++ codebook/Graph/CentroidDecomp.cpp
@@ -1,12 +1,12 @@
 vector<vector<int>> dis;
-vector<int> pa, sz;
+vector<int> pa, sz, dep;
 vector<bool> vis;
 void dfs_sz(int i, int p) {
   sz[i] = 1;
   for (int j : g[i]) if (j != p && !vis[j])
     dfs_sz(j, i), sz[i] += sz[j];
 }
-void cen(int i, int p, int _n) {
+int cen(int i, int p, int _n) {
   for (int j : g[i]) if (j != p && !vis[j] && sz[j] > _n / 2)
     return cen(j, i, _n);
   return i;
@@ -18,7 +18,7 @@ void dfs_dis(int i, int p, int d) { // from i to ancestor with depth d
 }
 void cd(int i, int p, int d) {
   dfs_sz(i), i = cen(i, -1, sz[i]);
-  vis[i] = true, pa[i] = p;
+  vis[i] = true, pa[i] = p, dep[i] = d;
   dfs_dis(i, -1, d);
   for (int j : g[i]) if (!vis[j])
     cd(j, i, d + 1);
