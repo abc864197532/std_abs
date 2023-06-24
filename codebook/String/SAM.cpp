8e37ec4717c08616aeb4e27f47c78718070e4b5c
abc864197532 <kimi930130@gmail.com>
2023-06-24 19:37:30 +0800

    Update SAM and remove pseg

--- codebook/String/SAM.cpp
+++ codebook/String/SAM.cpp
@@ -1,12 +1,19 @@
 struct SAM {
-  int ch[N][26], len[N], link[N], cnt[N], sz;
-  // link -> suffix endpos
-  SAM () {len[0] = 0, link[0] = -1, sz = 1;}
+  int ch[N][26], len[N], link[N], pos[N], cnt[N], sz;
+  // node -> strings with the same endpos set
+  // length in range [len(link) + 1, len]
+  // node's endpos set -> pos in the subtree of node
+  // link -> longest suffix with different endpos set
+  // len  -> longest suffix
+  // pos  -> end position
+  // cnt  -> size of endpos set
+  SAM () {len[0] = 0, link[0] = -1, pos[0] = 0, cnt[0] = 0, sz = 1;}
   void build(string s) {
     int last = 0;
-    for (char c : s) {
+    for (int i = 0; i < s.length(); ++i) {
+      char c = s[i];
       int cur = sz++;
-      len[cur] = len[last] + 1;
+      len[cur] = len[last] + 1, pos[cur] = i + 1;
       int p = last;
       while (~p && !ch[p][c - 'a']) ch[p][c - 'a'] = cur, p = link[p];
       if (p == -1) {
@@ -17,7 +24,7 @@ struct SAM {
           link[cur] = q;
         } else {
           int nxt = sz++;
-          len[nxt] = len[p] + 1, link[nxt] = link[q];
+          len[nxt] = len[p] + 1, link[nxt] = link[q], pos[nxt] = 0;
           for (int j = 0; j < 26; ++j) ch[nxt][j] = ch[q][j];
           while (~p && ch[p][c - 'a'] == q) ch[p][c - 'a'] = nxt, p = link[p];
           link[q] = link[cur] = nxt;
@@ -31,4 +38,4 @@ struct SAM {
     sort(all(p), [&](int i, int j) {return len[i] > len[j];});
     for (int i = 0; i < sz; ++i) cnt[link[p[i]]] += cnt[p[i]];
   }
-};
+} sam;
