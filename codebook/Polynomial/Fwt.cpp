6095195ce81bf0e20bed011ea0115e238cf3282b
abc864197532 <kimi930130@gmail.com>
2023-07-12 19:25:09 +0800

    update fwt

--- codebook/Polynomial/Fwt.cpp
+++ codebook/Polynomial/Fwt.cpp
@@ -1,10 +1,8 @@
 void fwt(vector <int> &a) {
-  // and : a[j] += x;
-  //     : a[j] -= x;
-  // or  : a[j ^ (1 << i)] += y;
-  //     : a[j ^ (1 << i)] -= y;
-  // xor : a[j] = x - y, a[j ^ (1 << i)] = x + y;
-  //     : a[j] = (x - y) / 2, a[j ^ (1 << i)] = (x + y) / 2;
+  // and : x += y * (1, -1)
+  // or  : y += x * (1, -1)
+  // xor : x = (x + y) * (1, 1/2)
+  //       y = (x - y) * (1, 1/2)
   int n = __lg(a.size());
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < 1 << n; ++j) if (j >> i & 1) {
@@ -23,14 +21,13 @@ vector<int> subs_conv(vector<int> a, vector<int> b) {
     ha[__builtin_popcount(i)][i] = a[i];
     hb[__builtin_popcount(i)][i] = b[i];
   }
-  for (int i = 0; i <= n; ++i) fwt(ha[i]),fwt(hb[i]);
+  for (int i = 0; i <= n; ++i) or_fwt(ha[i]), or_fwt(hb[i]);
   for (int i = 0; i <= n; ++i)
     for (int j = 0; i + j <= n; ++j)
       for (int k = 0; k < 1 << n; ++k)
         // mind overflow
         c[i + j][k] += ha[i][k] * hb[j][k]; 
-  for (int i = 0; i <= n; ++i)
-    fwt(c[i], true);
+  for (int i = 0; i <= n; ++i) or_fwt(c[i], true);
   vector <int> ans(1 << n);
   for (int i = 0; i < 1 << n; ++i)
     ans[i] = c[__builtin_popcount(i)][i];
