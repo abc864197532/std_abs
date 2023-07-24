1d67a2a4d8764ef7934bc78d6482149bd8a13df0
abc864197532 <kimi930130@gmail.com>
2023-07-25 01:41:43 +0800

    add FFT.cpp

---
+++ codebook/Polynomial/FFT.cpp
@@ -0,0 +1,19 @@
+using T = complex <double>;
+const double PI = acos(-1);
+struct NTT {
+  T w[N];
+  FFT() {
+    T dw = {cos(2 * PI / N), sin(2 * PI / N)};
+    w[0] = 1;
+    for (int i = 1; i < N; ++i) w[i] = w[i - 1] * dw;
+  }
+  void operator()(vector<T>& a, bool inv = false) {
+    // see NTT, replace ll with T
+    if (inv) {
+      reverse(a.begin() + 1, a.end());
+      T invn = 1.0 / n;
+      for (int i = 0; i < n; ++i) a[i] = a[i] * invn;
+    }
+  }
+} ntt;
+// after mul, round i.real()
