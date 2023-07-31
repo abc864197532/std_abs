85ee6475fff8d197c88624b491c45f03e0150673
abc864197532 <kimi930130@gmail.com>
2023-08-01 01:18:07 +0800

    update RotatingSweepLine and HalfPlaneIntersection

--- codebook/Geometry/HalfPlaneIntersection.cpp
+++ codebook/Geometry/HalfPlaneIntersection.cpp
@@ -1,26 +1,25 @@
-vector <Pt> HalfPlaneInter(vector <pair <Pt, Pt>> vec) {
-  //          x
-  // first -------> second
+vector<Pt> HalfPlaneInter(vector<Line> vec) {
+  //           x
+  // line.a -------> line.b
+  int n = vec.size();
   auto pos = [&](Pt a) {return sign(a.y) == 0 ?  sign(a.x) < 0 : sign(a.y) > 0;};
-  sort(all(vec), [&](pair <Pt, Pt> a, pair <Pt, Pt> b) {
-    Pt A = a.second - a.first, B = b.second - b.first;
+  sort(all(vec), [&](Line a, Line b) {
+    Pt A = a.b - a.a, B = b.b - b.a;
     if (pos(A) == pos(B)) {
-      if (sign(A ^ B) == 0) return sign((b.first - a.first) * (b.second - a.first)) > 0;
+      if (sign(A ^ B) == 0) return sign((b.a - a.a) * (b.b - a.a)) > 0;
       return sign(A ^ B) > 0;
     }
     return pos(A) < pos(B);
   });
   deque <Pt> inter;
-  deque <pair <Pt, Pt>> seg;
-  int n = vec.size();
-  auto get = [&](pair <Pt, Pt> a, pair <Pt, Pt> b) {return intersect(a.first, a.second, b.first, b.second);};
-  for (int i = 0; i < n; ++i) if (!i || vec[i] != vec[i - 1]) {
-    while (seg.size() >= 2 && sign((vec[i].second - inter.back()) ^ (vec[i].first - inter.back())) == 1) seg.pop_back(), inter.pop_back();
-    while (seg.size() >= 2 && sign((vec[i].second - inter.front()) ^ (vec[i].first - inter.front())) == 1) seg.pop_front(), inter.pop_front();
-    seg.push_back(vec[i]);
-    if (seg.size() >= 2) inter.pb(get(seg[seg.size() - 2], seg.back()));
+  deque <Line> seg;
+  for (int i = 0; i < n; ++i) if (!i || !(vec[i] == vec[i - 1])) {
+    while (seg.size() >= 2 && sign((vec[i].b - inter.back()) ^ (vec[i].a - inter.back())) == 1) seg.pop_back(), inter.pop_back();
+    while (seg.size() >= 2 && sign((vec[i].b - inter.front()) ^ (vec[i].a - inter.front())) == 1) seg.pop_front(), inter.pop_front();
+    seg.pb(vec[i]);
+    if (seg.size() >= 2) inter.pb(LinesInter(seg[seg.size() - 2], seg.back()));
   }
-  while (seg.size() >= 2 && sign((seg.front().second - inter.back()) ^ (seg.front().first - inter.back())) == 1) seg.pop_back(), inter.pop_back();
-  inter.push_back(get(seg.front(), seg.back()));
-  return vector <Pt>(all(inter));
+  while (seg.size() >= 2 && sign((seg.front().b - inter.back()) ^ (seg.front().a - inter.back())) == 1) seg.pop_back(), inter.pop_back();
+  inter.pb(LinesInter(seg.front(), seg.back()));
+  return vector<Pt>(all(inter));
 }
--- codebook/Geometry/RotatingSweepLine.cpp
+++ codebook/Geometry/RotatingSweepLine.cpp
@@ -1,21 +1,21 @@
 void RotatingSweepLine(vector <Pt> &pt) {
   int n = pt.size();
-  vector <int> ord(n), pos(n);
+  vector <int> ord(n), cur(n);
   vector <pii> line;
   for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) if (i ^ j)
     line.emplace_back(i, j);
-  auto in = [&](Pt a) {return sign(a.y) == 0 ? sign(a.x) < 0 : sign(a.y) > 0;};
+  auto pos = [&](Pt a) {return sign(a.y) == 0 ? sign(a.x) < 0 : sign(a.y) > 0;};
   sort(all(line), [&](pii i, pii j) {
     Pt a = pt[i.second] - pt[i.first], b = pt[j.second] - pt[j.first];
-    return (in(a) == in(b) ? sign(a ^ b) > 0 : in(a) < in(b));
+    return (pos(a) == pos(b) ? sign(a ^ b) > 0 : pos(a) < pos(b));
   });
   iota(all(ord), 0);
   sort(all(ord), [&](int i, int j) {
     return (sign(pt[i].y - pt[j].y) == 0 ? pt[i].x < pt[j].x : pt[i].y < pt[j].y);
   });
-  for (int i = 0; i < n; ++i) pos[ord[i]] = i;
+  for (int i = 0; i < n; ++i) cur[ord[i]] = i;
   for (auto [i, j] : line) {
     // point sort by the distance to line(i, j)
-    tie(pos[i], pos[j], ord[pos[i]], ord[pos[j]]) = make_tuple(pos[j], pos[i], j, i);
+    tie(cur[i], cur[j], ord[cur[i]], ord[cur[j]]) = make_tuple(cur[j], cur[i], j, i);
   }
 }
