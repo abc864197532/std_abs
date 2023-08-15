e49c0854803d552d8378699bc730f1bc57f916dd
abc864197532 <kimi930130@gmail.com>
2023-08-16 02:54:08 +0800

    add BinaryLifting

---
+++ codebook/Else/BinaryLifting.cpp
@@ -0,0 +1,11 @@
+int dep[N], pa[N], to[N]; // pa[rt] = rt, to[rt] = rt
+int lift(int x, int k) {
+  k = dep[x] - k;
+  while (dep[x] > k)
+  	x = dep[to[x]] < k ? pa[x] : to[x];
+  return x;
+}
+void add(int p, int v) {
+  dep[v] = dep[p] + 1, par[v] = p;
+  to[v] = dep[p] - dep[to[p]] == dep[to[p]] - dep[to[to[p]]] ? to[to[p]] : p;
+}
