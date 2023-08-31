0fbd852e1bfe922d1690b15c0c2baf91c205f6ac
abc864197532 <kimi930130@gmail.com>
2023-08-31 12:12:17 +0800

    Update Smawk.cpp

--- codebook/Else/Smawk.cpp
+++ codebook/Else/Smawk.cpp
@@ -1,70 +1,39 @@
-ll query(int l, int r) {
-  // ...
+ll f(int l, int r) { }
+bool select(int r, int u, int v) {
+  // if f(r, v) is better than f(r, v), return true
+  return f(r, u) < f(r, v);
 }
-struct SMAWK {
-  // Condition:
-  // If M[1][0] < M[1][1] then M[0][0] < M[0][1]
-  // If M[1][0] == M[1][1] then M[0][0] <= M[0][1]
-  // For all i, find r_i s.t. M[i][r_i] is maximum || minimum.
-  int ans[N], tmp[N];
-  void interpolate(vector <int> l, vector <int> r) {
-    int n = l.size(), m = r.size();
-    vector <int> nl;
-    for (int i = 1; i < n; i += 2) {
-      nl.push_back(l[i]);
-    }
-    run(nl, r);
-    for (int i = 1, j = 0; i < n; i += 2) {
-      while (j < m && r[j] < ans[l[i]])
-        j++;
-      assert(j < m && ans[l[i]] == r[j]);
-      tmp[l[i]] = j;
-    }
-    for (int i = 0; i < n; i += 2) {
-      int curl = 0, curr = m - 1;
-      if (i)
-        curl = tmp[l[i - 1]];
-      if (i + 1 < n)
-        curr = tmp[l[i + 1]];
-      ll res = query(l[i], r[curl]);
-      ans[l[i]] = r[curl];
-      for (int j = curl + 1; j <= curr; ++j) {
-        ll nxt = query(l[i], r[j]);
-        if (res < nxt)
-          res = nxt, ans[l[i]] = r[j];
-      }
+// For all 2x2 submatrix:
+// If M[1][0] < M[1][1], M[0][0] < M[0][1]
+// If M[1][0] == M[1][1], M[0][0] <= M[0][1]
+// M[i][ans_i] is the best value in the i-th row
+vector<int> solve(vector<int> &r, vector<int> &c) {
+  const int n = r.size();
+  if (n == 0) return {};
+  vector <int> c2;
+  for (const int &i : c) {
+    while (!c2.empty() && select(r[c2.size() - 1], c2.back(), i)) c2.pop_back();
+    if (c2.size() < n) c2.pb(i);
+  }
+  vector <int> r2;
+  for (int i = 1; i < n; i += 2) r2.pb(r[i]);
+  const auto a2 = solve(r2, c2);
+  vector <int> ans(n);
+  for (int i = 0; i < a2.size(); i++)
+    ans[i * 2 + 1] = a2[i];
+  int j = 0;
+  for (int i = 0; i < n; i += 2) {
+    ans[i] = c2[j];
+    const int end = i + 1 == n ? c2.back() : ans[i + 1];
+    while (c2[j] != end) {
+      j++;
+      if (select(r[i], ans[i], c2[j])) ans[i] = c2[j];
     }
   }
-  void reduce(vector <int> l, vector <int> r) {
-    int n = l.size(), m = r.size();
-    vector <int> nr;
-    for (int j : r) {
-      while (!nr.empty()) {
-        int i = nr.size() - 1;
-        if (query(l[i], nr.back()) <= query(l[i], j))
-          nr.pop_back();
-        else
-          break;
-      }
-      if (nr.size() < n)
-        nr.push_back(j);
-    }
-    run(l, nr);
-  }
-  void run(vector <int> l, vector <int> r) {
-    int n = l.size(), m = r.size();
-    if (max(n, m) <= 2) {
-      for (int i : l) {
-        ans[i] = r[0];
-        if (m > 1) {
-          if (query(i, r[0]) < query(i, r[1]))
-            ans[i] = r[1];
-        }
-      }
-    } else if (n >= m) {
-      interpolate(l, r);
-    } else {
-      reduce(l, r);
-    }
-  }
-};
+  return ans;
+}
+vector<int> smawk(int n, int m) {
+  vector<int> row(n), col(m);
+  iota(all(row), 0), iota(all(col), 0);
+  return solve(row, col);
+}
