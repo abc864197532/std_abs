818a858479b2af44348f62ed0235834c43b45d81
abc864197532 <kimi930130@gmail.com>
2023-09-10 02:37:39 +0800

    add DiscreteLog.cpp

---
+++ codebook/Math/DiscreteLog.cpp
@@ -0,0 +1,18 @@
+ll DiscreteLog(ll a, ll b, ll m) {
+  const int B = 35000;
+  ll k = 1 % m, ans = 0, g;
+  while ((g = gcd(a, m)) > 1) {
+    if (b == k) return ans;
+    if (b % g) return -1;
+    b /= g, m /= g, ans++, k = (k * a / g) % m;
+  }
+  if (b == k) return ans;
+  unordered_map <ll, int> m1;
+  ll tot = 1;
+  for (int i = 0; i < B; ++i)
+    m1[tot * b % m] = i, tot = tot * a % m;
+  ll cur = k * tot % m;
+  for (int i = 1; i <= B; ++i, cur = cur * tot % m)
+    if (m1.count(cur)) return i * B - m1[cur] + ans;
+  return -1;
+}
