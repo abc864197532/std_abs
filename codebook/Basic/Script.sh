7fd022f9391f04a7d24b4e50a7bd30a6be25d5ed
abc864197532 <kimi930130@gmail.com>
2023-09-12 03:08:22 +0800

    update script

--- codebook/Basic/Script.sh
+++ codebook/Basic/Script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-g++ -std=c++17 -DABS -Wall -Wextra -Wshadow $1.cpp -o $1 && ./$1
+g++ -std=c++17 -DABS -O2 -Wall -Wextra -Wshadow $1.cpp -o $1 && ./$1
 for i in {A..J}; do cp tem.cpp $i.cpp; done;
