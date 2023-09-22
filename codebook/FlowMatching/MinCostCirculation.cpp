de228a602e5f17836b7762a53365f4d61396b669
abc864197532 <kimi930130@gmail.com>
2023-09-22 12:12:10 +0800

    add MinCostCirculation

---
+++ codebook/FlowMatching/MinCostCirculation.cpp
@@ -0,0 +1,55 @@
+struct MinCostCirculation { // 0-base
+  struct Edge {
+    ll from, to, cap, fcap, flow, cost, rev; 
+  } *past[N];
+  vector<Edge> G[N];
+  ll dis[N], inq[N], n;
+  void BellmanFord(int s) {
+    fill_n(dis, n, INF), fill_n(inq, n, 0);
+    queue<int> q;
+    auto relax = [&](int u, ll d, Edge *e) {
+      if (dis[u] > d) {
+        dis[u] = d, past[u] = e;
+        if (!inq[u]) inq[u] = 1, q.push(u);
+      }
+    };
+    relax(s, 0, 0);
+    while (!q.empty()) {
+      int u = q.front();
+      q.pop(), inq[u] = 0;
+      for (auto &e : G[u])
+        if (e.cap > e.flow)
+          relax(e.to, dis[u] + e.cost, &e);
+    }
+  }
+  void try_edge(Edge &cur) {
+    if (cur.cap > cur.flow) return ++cur.cap, void();
+    BellmanFord(cur.to);
+    if (dis[cur.from] + cur.cost < 0) {
+      ++cur.flow, --G[cur.to][cur.rev].flow;
+      for (int i = cur.from; past[i]; i = past[i]->from) {
+        auto &e = *past[i];
+        ++e.flow, --G[e.to][e.rev].flow;
+      }
+    }
+    ++cur.cap;
+  }
+  void solve(int mxlg) {
+    for (int b = mxlg; b >= 0; --b) {
+      for (int i = 0; i < n; ++i)
+        for (auto &e : G[i])
+          e.cap *= 2, e.flow *= 2;
+      for (int i = 0; i < n; ++i)
+        for (auto &e : G[i])
+          if (e.fcap >> b & 1)
+            try_edge(e);
+    }
+  }
+  void init(int _n) { n = _n;
+    for (int i = 0; i < n; ++i) G[i].clear();
+  }
+  void add_edge(ll a, ll b, ll cap, ll cost) {
+    G[a].pb(Edge{a, b, 0, cap, 0, cost, sz(G[b]) + (a == b)});
+    G[b].pb(Edge{b, a, 0, 0, 0, -cost, sz(G[a]) - 1});
+  }
+} mcmf; // O(VE * ElogC)
