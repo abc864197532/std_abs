da4d16d8cd38bd53dc7142bb1f94e5fd163752de
abc864197532 <kimi930130@gmail.com>
2023-10-20 19:28:03 +0800

    update Python Misc

--- codebook/Else/python.py
+++ codebook/Else/python.py
@@ -10,10 +10,14 @@ Fraction(Decimal('1.14'))
 Fraction('1.2').limit_denominator(4).numerator
 Fraction(cos(pi / 3)).limit_denominator()
 # set
-S = set() S.add((a, b)) S.remove((a, b))
+S = set()
+S.add((a, b))
+S.remove((a, b))
 if not (a, b) in S:
 # dict
-D = dict() D[(a, b)] = 1 del D[(a, b)]
+D = dict()
+D[(a, b)] = 1
+del D[(a, b)]
 for (a, b) in D.items():
 # random
 arr = [randint(1, C) for i in range(N)]
