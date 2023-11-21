85ce299863ac5d225562ce38c97df16d6060bf83
abc864197532 <kimi930130@gmail.com>
2023-11-22 01:41:16 +0800

    reduce python note's line

--- codebook/Else/python.py
+++ codebook/Else/python.py
@@ -1,27 +1,16 @@
 from [decimal, fractions, math, random] import *
-# input
-arr = list(map(int, input().split())) 
-# decimal
+arr = list(map(int, input().split())) # input
 setcontext(Context(prec=10, Emax=MAX_EMAX, rounding=ROUND_FLOOR))
 Decimal('1.1') / Decimal('0.2')
-# fractions
 Fraction(3, 7)
 Fraction(Decimal('1.14'))
 Fraction('1.2').limit_denominator(4).numerator
 Fraction(cos(pi / 3)).limit_denominator()
-# set
-S = set()
-S.add((a, b))
-S.remove((a, b))
+S = set(), S.add((a, b)), S.remove((a, b)) # set
 if not (a, b) in S:
-# dict
-D = dict()
-D[(a, b)] = 1
-del D[(a, b)]
+D = dict(), D[(a, b)] = 1, del D[(a, b)] # dict
 for (a, b) in D.items():
-# random
 arr = [randint(1, C) for i in range(N)]
 choice([8, 6, 4, 1]) # random pick one
-shuffle(arr) #shuffle a list
-# print array
+shuffle(arr)
 print(*arr, sep=' ')
