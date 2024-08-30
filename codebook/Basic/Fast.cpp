8950179bbf0542dc5fa5737ef5d9dca7437486d8
abc864197532 <kimi930130@gmail.com>
2024-08-30 20:56:59 +0800

    Update Pragma

--- codebook/Basic/Fast.cpp
+++ codebook/Basic/Fast.cpp
@@ -1,5 +1,8 @@
-#pragma GCC optimize("Ofast,inline,unroll-loops")
-#pragma GCC target("bmi,bmi2,lzcnt,popcnt,avx2")
+#pragma GCC optimize("Ofast,no-stack-protector")
+#pragma GCC optimize("no-math-errno,unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4")
+#pragma GCC target("popcnt,abm,mmx,avx,arch=skylake")
+__builtin_ia32_ldmxcsr(__builtin_ia32_stmxcsr()|0x8040)
 #include<unistd.h>
 char OB[65536]; int OP;
 inline char RC() {
