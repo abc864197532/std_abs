4cf254abc229278368fbdbf9706781dbf811728e
abc864197532 <kimi930130@gmail.com>
2024-08-30 15:22:34 +0800

    Fix Else

--- codebook/Else/BitHack.cpp
+++ codebook/Else/BitHack.cpp
@@ -1,4 +1,3 @@
-long long next_perm(long long v) {
-  long long t = v | (v - 1);
-  return (t + 1) | (((~t & -~t) - 1) >> (__builtin_ctz(v) + 1));
-}
+ll next_perm(ll v) { ll t = v | (v - 1);
+  return (t + 1) |
+    (((~t & -~t) - 1) >> (__builtin_ctz(v) + 1)); }
--- codebook/Else/MinPlusConvolution.cpp
+++ codebook/Else/MinPlusConvolution.cpp
@@ -1,6 +1,6 @@
 // a is convex a[i+1]-a[i] <= a[i+2]-a[i+1]
 vector<int> min_plus_convolution(vector<int> &a, vector<int> &b) {
-  int n = SZ(a), m = SZ(b);
+  int n = a.size(), m = b.size();
   vector<int> c(n + m - 1, INF);
   auto dc = [&](auto Y, int l, int r, int jl, int jr) {
     if (l > r) return;
@@ -9,7 +9,8 @@ vector<int> min_plus_convolution(vector<int> &a, vector<int> &b) {
       if (int i = mid - j; i >= 0 && i < n)
         if (best > a[i] + b[j]) 
           best = a[i] + b[j], from = j;
-    Y(Y, l, mid - 1, jl, from), Y(Y, mid + 1, r, from, jr);
+    Y(Y, l, mid - 1, jl, from);
+    Y(Y, mid + 1, r, from, jr);
   };
   return dc(dc, 0, n - 1 + m - 1, 0, m - 1), c;
 }
