4792ce73cd45438817da4ae1908644dbd2994012
abc864197532 <kimi930130@gmail.com>
2024-08-31 03:23:22 +0800

    Add Lyndon Factorization

---
+++ codebook/String/LyndonFactorization.cpp
@@ -0,0 +1,13 @@
+// partition s = w[0] + w[1] + ... + w[k-1],
+// w[0] >= w[1] >= ... >= w[k-1]
+// each w[i] strictly smaller than all its suffix
+vector <string> duval(const string &s) {
+  vector <string> ans;
+  for (int n = (int)s.size(), i = 0, j, k; i < n; ) {
+    for (j = i + 1, k = i; j < n && s[k] <= s[j]; j++)
+      k = (s[k] < s[j] ? i : k + 1);
+    for (; i <= k; i += j - k)
+      ans.pb(s.substr(i, j - k)); // s.substr(l, len)
+  }
+  return ans;
+}
