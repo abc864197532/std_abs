eabc78bf24d038aa30f8ddb7213fe5f5dc24225c
abc864197532 <kimi930130@gmail.com>
2024-08-30 21:16:34 +0800

    Update Basic

--- codebook/Basic/Div.cpp
+++ codebook/Basic/Div.cpp
@@ -1,9 +1,5 @@
-ll floor(ll a, ll b) {
-  return a / b - (a < 0 && a % b);
-}
-ll ceil(ll a, ll b) {
-  return a / b + (a > 0 && a % b);
-}
+ll floor(ll a, ll b) {return a / b - (a < 0 && a % b);}
+ll ceil(ll a, ll b)  {return a / b + (a > 0 && a % b);}
 a / b <  x -> floor(a, b) + 1 <= x
 a / b <= x -> ceil(a, b) <= x
 x <  a / b -> x <= ceil(a, b) - 1
