c4ea609f6d9c3cb881af449c44a94495db60668d
abc864197532 <kimi930130@gmail.com>
2024-11-27 14:53:54 +0800

    update codebook

---
+++ codebook/Geometry/3DPointSimple.cpp
@@ -0,0 +1,27 @@
+struct Pt {
+  double x, y, z; // + - * / write yourself
+  double operator * (const Pt &o) const
+  { return x * o.x + y * o.y + z * o.z; }
+  Pt operator ^ (const Pt &o) const
+  { return {Pt(y * o.z - z * o.y, z * o.x - x * o.z, x * o.y - y * o.x)}; }
+};
+double abs2(Pt o) { return o * o; }
+double abs(Pt o) { return sqrt(abs2(o)); }
+Pt cross3(Pt a, Pt b, Pt c)
+{ return (b - a) ^ (c - a); }
+double area(Pt a, Pt b, Pt c)
+{ return abs(cross3(a, b, c)); }
+double volume(Pt a, Pt b, Pt c, Pt d)
+{ return cross3(a, b, c) * (d - a); }
+bool coplaner(Pt a, Pt b, Pt c, Pt d)
+{ return sign(volume(a, b, c, d)) == 0; }
+Pt proj(Pt o, Pt a, Pt b, Pt c) // o proj to plane abc
+{ Pt n = cross3(a, b, c);
+  return o - n * ((o - a) * (n / abs2(n)));}
+Pt LinePlaneInter(Pt u, Pt v, Pt a, Pt b, Pt c) {
+  // intersection of line uv and plane abc
+  Pt n = cross3(a, b, c);
+  double s = n * (u - v);
+  if (sign(s) == 0) return {-1, -1, -1}; // not found
+  return v + (u - v) * ((n * (a - v)) / s);
+}
