a3f20029856c6db231e14efd5bbefa6c99389b02
abc864197532 <kimi930130@gmail.com>
2025-03-17 01:51:28 +0800

    update debugmacro

--- codebook/Basic/DefaultCode_Full.cpp
+++ codebook/Basic/DefaultCode_Full.cpp
@@ -6,17 +6,19 @@ typedef pair<int, int> pii;
 #define all(a) a.begin(), a.end()
 #define sz(a) ((int)a.size())
 #ifdef ABS
-void db() {}
 template <typename T>
 ostream& operator << (ostream &o, vector <T> vec) {
     o << "{"; int f = 0;
     for (T i : vec) o << (f++ ? " " : "") << i;
     return o << "}"; }
-template <typename T, typename ...U> void db(T i, U ...j) { cerr << i << " ", db(j...); }
-#define ppr(c, x...) cerr << "\e[1;" << c << "m", db(__LINE__, "[" + string(#x) + "]", x), cerr << "\e[0m" << endl
-#define bug(x...) ppr(32, x)
-#define bugv(x...) ppr(36, vector(x))
-#define safe ppr(33, "safe")
+void bug__(int c, auto ...a) {
+    cerr << "\e[1;" << c << "m";
+    (..., (cerr << a << " "));
+    cerr << "\e[0m" << endl; }
+#define bug_(c, x...) bug__(c, __LINE__, "[" + string(#x) + "]", x)
+#define bug(x...) bug_(32, x)
+#define bugv(x...) bug_(36, vector(x))
+#define safe bug_(33, "safe")
 #else
 #define bug(x...) void(0)
 #define bugv(x...) void(0)
