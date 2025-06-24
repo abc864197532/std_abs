d3b57e4452c3922d87f4099e8f94ccfda00b44d4
abc864197532 <kimi930130@gmail.com>
2025-06-25 00:05:37 +0800

    Add BasicSimple.cpp

---
+++ codebook/Geometry/BasicSimple.cpp
@@ -0,0 +1,30 @@
+template <typename T> struct P {};
+using Pt = P<ll>;
+struct Line { Pt a, b; };
+struct Cir { Pt o; double r; };
+ll abs2(Pt a) { return a * a; }
+double abs(Pt a) { return sqrt(abs2(a)); }
+int ori(Pt o, Pt a, Pt b)
+{ return sign((o - a) ^ (o - b)); }
+bool btw(Pt a, Pt b, Pt c) // c on segment ab?
+{ return ori(a, b, c) == 0 &&
+         sign((c - a) * (c - b)) <= 0; }
+int pos(Pt a)
+{ return sign(a.y) == 0 ? sign(a.x) < 0 : a.y < 0; }
+bool cmp(Pt a, Pt b)
+{ return pos(a) == pos(b) ? sign(a ^ b) > 0 :
+         pos(a) < pos(b); }
+bool same_vec(Pt a, Pt b)
+{ return sign(a ^ b) == 0 && sign(a * b) > 0; }
+Pt perp(Pt a) { return Pt(-a.y, a.x); } // CCW 90 deg
+// double part
+double theta(Pt a)
+{ return normalize(atan2(a.y, a.x)); }
+Pt unit(Pt o) { return o / abs(o); }
+Pt rot(Pt a, double o) // CCW
+{ double c = cos(o), s = sin(o);
+  return Pt(c * a.x - s * a.y, s * a.x + c * a.y); }
+Pt proj_vec(Pt a, Pt b, Pt c) // vector ac proj to ab
+{return (b - a) * ((c - a) * (b - a)) / (abs2(b - a));}
+Pt proj_pt(Pt a, Pt b, Pt c) // point c proj to ab
+{ return proj_vec(a, b, c) + a; }
