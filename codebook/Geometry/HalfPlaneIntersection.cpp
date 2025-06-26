f2eeb0a0f94ec8d14a97cb9f7dab9c24959702d9
abc864197532 <kimi930130@gmail.com>
2025-06-26 23:34:07 +0800

    Update HalfPlaneIntersection.cpp

--- codebook/Geometry/HalfPlaneIntersection.cpp
+++ codebook/Geometry/HalfPlaneIntersection.cpp
@@ -1,18 +1,20 @@
-auto area_pair(Line a, Line b) 
-{ return make_pair((a.b - a.a) ^ (b.a - a.a),
-                   (a.b - a.a) ^ (b.b - a.a)); }
-bool isin(Line l0, Line l1, Line l2) {
-  // Check inter(l1, l2) strictly in l0
-  auto [a02X, a02Y] = area_pair(l0, l2);
-  auto [a12X, a12Y] = area_pair(l1, l2);
-  if (a12X - a12Y < 0) a12X *= -1, a12Y *= -1;
-  return (__int128)a02Y * a12X - (__int128)a02X * a12Y > 0; // C^4
-}
 /* Having solution, check size > 2 */
 /* --^-- Line.a --^-- Line.b --^-- */
 auto halfplane_intersection(vector<Line> arr) {
+  auto area_pair = [&](Line a, Line b) {
+    return make_pair((a.b - a.a) ^ (b.a - a.a),
+                     (a.b - a.a) ^ (b.b - a.a)); };
+  auto isin = [&](Line l0, Line l1, Line l2) {
+    // Check inter(l1, l2) strictly in l0
+    auto [a02X, a02Y] = area_pair(l0, l2);
+    auto [a12X, a12Y] = area_pair(l1, l2);
+    if (a12X - a12Y < 0) a12X *= -1, a12Y *= -1;
+    return (__int128)a02Y * a12X - 
+           (__int128)a02X * a12Y > 0; // C^4
+  };
   sort(all(arr), [&](Line a, Line b) {
-    if (same_vec(a, b, 1)) return ori(a.a, a.b, b.b) < 0;
+    if (same_vec(a, b, 1))
+      return ori(a.a, a.b, b.b) < 0;
     return cmp(a.b - a.a, b.b - b.a); });
   deque<Line> dq(1, arr[0]);
   auto pop_back = [&](int t, Line p) {
