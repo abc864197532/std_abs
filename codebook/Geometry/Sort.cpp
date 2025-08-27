89e2b56ea5b20d995fc00a1f69d3590d291ae7d8
abc864197532 <kimi930130@gmail.com>
2025-08-27 23:18:54 +0800

    fix

--- codebook/Geometry/Sort.cpp
+++ codebook/Geometry/Sort.cpp
@@ -12,3 +12,25 @@ bool cmp_fraction_polar(pair<Pt, ll> o, pair<Pt, ll> s, pair<Pt, ll> t) { // C^3
   // u /= gcd(u.x, u.y) might lower the range to C
   return cmp(u, v);
 } // 2d4450
+struct Polar_Seg {
+  Pt a, b; // ori(Pt(0, 0), a, b) > 0
+  bool operator < (const Polar_Seg &o) const {
+    if (btwangle(Pt(0, 0), a, b, o.a, 0)) {
+      if (a == o.a) return ori(o.b, a, b) == -1;
+      if (b == o.a) return false;
+      return ori(o.a, a, b) == -1;
+    }
+    if (b == o.a) return false;
+    return ori(a, o.a, o.b) == 1;
+  }
+};
+struct Arc {
+	Cir c; int s; // 0 -> up, 1 -> down
+	bool operator < (const Arc &b) const {
+		if (c.id == b.c.id) return s < b.s;
+		if (contain(c, b.c)) return b.s == 1;
+		else if (contain(b.c, c)) return s == 0;
+		else if (c.o.y == b.c.o.y) return c.id < b.c.id;
+	  else return c.o.y > b.c.o.y;
+	}
+};
