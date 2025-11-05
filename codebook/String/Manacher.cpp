357ca92ceb2e8451e5311923bfd6b196798f7426
abc864197532 <kimi930130@gmail.com>
2025-11-06 03:09:12 +0800

    update manacher

--- codebook/String/Manacher.cpp
+++ codebook/String/Manacher.cpp
@@ -1,13 +1,14 @@
 // return value only consider string tmp, not s
 // return array length = 2N - 1
-auto manacher(string tmp) {
-  string s = "&";
-  for (char c : tmp) s.pb(c), s.pb('%');
+auto manacher(auto tmp) {
+  vector<int> s(1, -1);
+  for (auto c : tmp) s.pb(c), s.pb(-1);
   int l = 0, r = 0, n = sz(s);
-  vector <int> Z(n);
+  vector<int> Z(n);
   for (int i = 0; i < n; ++i) {
     Z[i] = r > i ? min(Z[2 * l - i], r - i) : 1;
-    while (s[i + Z[i]] == s[i - Z[i]]) Z[i]++;
+    while (i - Z[i] >= 0 && i + Z[i] < n && 
+           s[i + Z[i]] == s[i - Z[i]]) Z[i]++;
     if (Z[i] + i > r) l = i, r = Z[i] + i;
   }
   for (int i = 0; i < n; ++i)
