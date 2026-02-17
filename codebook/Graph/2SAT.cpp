668ef04ec1c1dcbfa875537ce5a276987534ff49
Kimi Wu <kimi930130@gmail.com>
2026-02-17 21:13:00 +0800

    update SAT

--- codebook/Graph/2SAT.cpp
+++ codebook/Graph/2SAT.cpp
@@ -1,15 +1,13 @@
 struct SAT { // 0-based, need SCC
-  int n; vector <pii> edge; vector <int> is;
+  int n; SCC scc; vector <int> is;
   int rev(int x) { return x < n ? x + n : x - n; }
   void add_ifthen(int x, int y) {
     add_clause(rev(x), y); }
   void add_clause(int x, int y) {
-    edge.emplace_back(rev(x), y);
-    edge.emplace_back(rev(y), x); }
+    scc.add_edge(rev(x), y);
+    scc.add_edge(rev(y), x); }
   bool solve() {
     // is[i] = true -> i, is[i] = false -> -i
-    SCC scc(2 * n);
-    for (auto [u, v] : edge) scc.add_edge(u, v);
     scc.build();
     for (int i = 0; i < n; ++i) {
       if (scc.scc_id[i] == scc.scc_id[i + n])
@@ -18,5 +16,5 @@ struct SAT { // 0-based, need SCC
     }
     return true;
   }
-  SAT (int _n) : n(_n), edge(), is(n) {}
+  SAT (int _n) : n(_n), scc(2 * n), is(n) {}
 };
